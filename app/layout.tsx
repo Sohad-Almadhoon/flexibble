c69c8fbfb8bc7c80ec5868a2914024f26cb89e0e
Sohad-Almadhoon <89332592+Sohad-Almadhoon@users.noreply.github.com>
2023-06-27 16:09:30 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import Navbar from '@/components/Navbar'
+import './globals.css'
+import Footer from '@/components/Footer';
+export const metadata = {
+  title: 'Flexibble',
+  description: 'Showcase and discover remarkable developer projects',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar/>
+        <main>{children}</main>
+        <Footer/>
+      </body>
+    </html>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,38 @@
+import { NavLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import AuthProvider from "./AuthProvider";
+
+const Navbar = () => {
+  const session = {};
+  return (
+    <nav>
+      <div className="navbar flexBetween">
+        <div className="flex-1 flexStart gap-10">
+          <Link href="/">
+            <Image src="/logo.svg" width={115} height={43} alt="Flexibble" />
+          </Link>
+          <ul className="xl:flex hidden text-small gap-7">
+            {NavLinks.map((link) => (
+              <Link href={link.href} key={link.key}>
+                {link.text}
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <div className="flexCenter gap-4">
+          {session ? (
+            <>
+              User Photo
+              <Link href="/create-project">Share Work</Link>
+            </>
+          ) : (
+            <AuthProvider />
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
