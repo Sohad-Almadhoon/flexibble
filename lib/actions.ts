46c61eaba72f75df34f991ea2a8e65f566ba68cd
Sohad-Almadhoon <89332592+Sohad-Almadhoon@users.noreply.github.com>
2023-07-08 16:57:09 +0300

    d

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,8 +4,8 @@ import { GraphQLClient } from "graphql-request";
 
 
 const isProduction = process.env.NODE_ENV === "production";
-const apiUrl = isProduction ? process.env.GRAFBASE_API_URL || '' : " http://127.0.0.1:4000/graphql";
-const apiKey = isProduction ? process.env.GRAFBASE_API_KEY || '' : "1234";
+const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : " http://127.0.0.1:4000/graphql";
+const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : "1234";
 const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : "http://localhost:3000";
 console.log(`${serverUrl}/api/auth/token`)
 
