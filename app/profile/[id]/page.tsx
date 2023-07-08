2e791efddc58b7cd4387b7f46b7634c105966417
Sohad-Almadhoon <89332592+Sohad-Almadhoon@users.noreply.github.com>
2023-07-08 13:39:08 +0300

    ...

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -5,7 +5,7 @@ import { getUserProjects } from "@/lib/actions";
 const UserProfile = async ({ params: { id } }: { params: { id: string } }) => {
   const result = (await getUserProjects(id, 100)) as { user: UserProfile };
   if (!result)
-    return <p className="no-result-text">Failed to fetch user Info</p>;
+    return <p className="no-result-text">Failed to fetch user Info...</p>;
     return <Profile user={result?.user} />;
 };
 
