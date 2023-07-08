553bebd1bf141b2c581611194172e4e47db4a913
Sohad-Almadhoon <89332592+Sohad-Almadhoon@users.noreply.github.com>
2023-07-08 15:39:04 +0300

    add image optimization

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com", "res.cloudinary.com"],
+    domains: [
+      "lh3.googleusercontent.com",
+      "res.cloudinary.com",
+      "https://flexibble-sand.vercel.app",
+    ],
   },
   experimental: {
     serverComponentsExternalPackages: ["cloudinary", "graphql-request"],
   },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
